5fe4510eb983915ffe3fb393a82cc8a5b8c52451
M Fadhly NR <33947296+maslow123@users.noreply.github.com>
2019-08-01 22:28:37 +0700

    there are some changes to the first and second files

---
+++ task_one.go
@@ -0,0 +1,41 @@
+package main
+import (
+	"fmt" 
+	"sort" 
+	"strings"
+)
+
+func main() {
+	sortWord("osama")
+}
+
+func sortWord(word string) {
+	var arrVowel, arrConsonant []string
+	
+	param := strings.Replace(word," ","",-1)
+	// Pecahkan nilai string parameter menjadi array
+	arrWord := strings.Split(param,"")
+
+	vowels := [5]string {"a","i","u","e","o"}// [ a i u e o ]
+	// Lakukan perulangan untuk mendapatkan nilai setiap index arrWord.
+	for _,value := range arrWord {
+		// Lakukan perulangan lagi untuk mendapatkan nilai setiap index dari vokal
+		for index,v := range vowels {
+			// Cek jika ada value yang sama dengan v, maka masukkan  value kedalam array arrVowel untuk mendapatkan huruf hidup
+			if value == v {
+				arrVowel = append(arrVowel, value)
+				break
+			// Lanjut kekondisi kedua untuk mendapatkan huruf mati yang ada pada nilai parameter
+			} else if index == len(vowels) - 1{
+				arrConsonant = append(arrConsonant, value)
+				// fmt.Println(arrConsonant)
+			}
+		}
+	}
+	sort.Strings(arrVowel) 
+	sort.Strings(arrConsonant)
+
+	result := strings.Join(arrVowel,"") + strings.Join(arrConsonant,"")
+	
+	fmt.Printf(result)
+}
---
+++ task_zero.go
@@ -0,0 +1,50 @@
+package main
+
+import(
+	"fmt"
+	"strings"
+)
+
+func main() {
+	vowelAndConsonant("omama")
+}
+
+func vowelAndConsonant(w string) {
+	var arrVowel, arrConsonant []string
+	param := strings.Replace(w," ","",-1)
+
+	// Pecahkan nilai string parameter menjadi array
+	arrWord := strings.Split(param,"")
+	
+	vowel := [5]string{"a","i","u","e","o"} // [ a i u e o ]
+	for _, value := range arrWord {
+		// fmt.Println(value)
+		for index,v := range vowel {
+			// cek apakah value ada yang sama dengan v atau tidak, jika ada maka lakukan sortir dan kekondisi selanjutnya
+			if value == v {
+				// cek lagi jika ada index array yang tidak nol, maka lakukan perulangan lagi untuk mendapatkan huruf hidup
+				if len(arrVowel) != 0 {
+					for u,y := range arrVowel {
+						// cek apakah ada huruf hidup yang sama, jika ada maka kondisi akan selesai dan berlanjut ke kondisi berikutnya
+						if value == y {
+							break
+						} else if u == len(arrVowel) - 1 {
+							// Jika u sama dengan panjang arrVowel, maka masukkan nilai array yang ada pada variable value kedalam variable arrVowel
+							arrVowel = append(arrVowel, value) // [ o , a ]
+						}
+					}
+				// jika panjang arrVowel nol, maka langsung masukkan nilai array yang ada pada variable value kedalam variable arrVowel
+				}else {
+					arrVowel = append(arrVowel, value)
+				}
+				break
+			// ambil banyaknya jumlah huruf mati yang ada pada nilai parameter
+			}else if index == len(vowel) -1 {
+				arrConsonant = append(arrConsonant, value)
+			}
+		}
+	}
+	
+	fmt.Printf("Huruf mati : %d\n",len(arrConsonant))
+	fmt.Printf("Huruf hidup: %d",len(arrVowel))
+}
